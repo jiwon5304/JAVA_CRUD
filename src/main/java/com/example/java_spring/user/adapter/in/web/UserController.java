99e24c39d80433251c39c6ac7024999e5e3aa245
jiwon5304 <80395324+jiwon5304@users.noreply.github.com>
2022-07-14 18:16:29 +0900

    UsersApi 사용

---
+++ src/main/java/com/example/java_spring/user/adapter/in/web/UserController.java
@@ -0,0 +1,8 @@
+package com.example.java_spring.user.adapter.in.web;
+
+import com.example.api.UsersApi;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController implements UsersApi {
+}
